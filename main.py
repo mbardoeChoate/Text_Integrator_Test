c9435bcf299ab1686b0079fac34d7a29b25278c0
Matthew Bardoe <mbardoe@gmail.com>
2023-03-21 17:34:45 -0400

    Code Cleanup

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import os
 import glob
+import os
 
-class Markdown_Builder:
 
-    def __init__(self, directory:str):
+class MarkdownBuilder:
+
+    def __init__(self, directory: str):
         self.directory = directory
-        self.current_directory=os.getcwd()
-        #print(self.current_directory)
-        #print(self.check_for_original())
+        self.current_directory = os.getcwd()
+        # print(self.current_directory)
+        # print(self.check_for_original())
 
     def delete_original(self):
         if self.check_for_original():
-            os.remove(self.current_directory+"/main.md")
+            os.remove(self.current_directory + "/main.md")
 
     def check_for_original(self):
         os.chdir(self.current_directory)
@@ -21,10 +22,9 @@ class Markdown_Builder:
         self.delete_original()
         os.chdir(self.directory)
         for f in glob.glob("*.md"):
-            os.system("cat " + f + " >> "+self.current_directory+"/main.md")
+            os.system("cat " + f + " >> " + self.current_directory + "/main.md")
 
 
-
-if __name__=="__main__":
-    mb=Markdown_Builder("Text_Snippets")
+if __name__ == "__main__":
+    mb = MarkdownBuilder("Text_Snippets")
     mb.combine_files()
